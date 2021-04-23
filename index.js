e05804260a0f61b2fb7d63db8ec22be440649a51
Zach Frohardt <zfrohardt@gmail.com>
2021-04-23 11:25:43 -0700

    completed assignment

--- index.js
+++ index.js
@@ -20,3 +20,69 @@ let allWagesFor = function () {
 
     return payable
 }
+
+let createEmployeeRecord = args => {
+    return {
+        firstName: args[0],
+        familyName: args[1],
+        title: args[2],
+        payPerHour: args[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+}
+
+let createEmployeeRecords = employees => {
+    return employees.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(timestamp) {
+    let ts = parseTimestamp(timestamp);
+    this.timeInEvents.push({
+        type: "TimeIn",
+        date: ts.date,
+        hour: ts.hour,
+    });
+
+    return this;
+}
+
+function createTimeOutEvent(timestamp) {
+    let ts = parseTimestamp(timestamp);
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        date: ts.date,
+        hour: ts.hour,
+    });
+
+    return this;
+}
+
+function parseTimestamp(timestamp) {
+    return {
+        date: timestamp.split(" ")[0],
+        hour: Number.parseInt(timestamp.split(" ")[1]),
+    }
+}
+
+function hoursWorkedOnDate(date) {
+    let timeIn = this.timeInEvents.find(timeInEvent => timeInEvent.date === date).hour;
+    let timeOut = this.timeOutEvents.find(timeOutEvent => timeOutEvent.date === date).hour;
+    return (timeOut - timeIn) / 100;
+}
+
+function wagesEarnedOnDate(date) {
+    return this.payPerHour * hoursWorkedOnDate.call(this, date);
+}
+
+function findEmployeeByFirstName(employees, firstName) {
+    return employees.find(employee => employee.firstName === firstName);
+}
+
+function calculatePayroll(employees) {
+    return employees.reduce((acc, employee) => {
+        return acc + employee.timeInEvents.reduce((acc, timeInEvent) => {
+            return acc + wagesEarnedOnDate.call(employee, timeInEvent.date)
+        }, 0)
+    }, 0)
+}
